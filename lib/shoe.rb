498b4459d1eb1098e227768b69f24cb67b5907cb
Uriel <u.e.rodriguez@gmail.com>
2020-02-22 21:11:23 -0500

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,8 +2,17 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
     @brand = brand
+
+    # BRANDS << brand if not BRANDS.include?(brand)
+    BRANDS.reduce(0) do |memo, array_brand| 
+      memo = array_brand != brand
+      # memo
+    end ? BRANDS << brand : nil 
+    
   end
 
   def cobble
